c7d109a2bc79f704713d2042eb72b2cb1034db08
Ursula Miller <ursula.miller+fi@gmail.com>
2019-04-14 15:29:07 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def display_board(board)
+  line = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts line
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts line
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+
+def input_to_index(user_input)
+  space = user_input.to_i - 1
+  return space
+end
+
+
+def turn()
+
